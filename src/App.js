34122f73904d540d3a5a3ac788ff05bb11268051
My Name <cpgsom819@gmail.com>
2023-03-16 06:17:24 +0800

    starting

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-// import logo from './logo.svg';
 import './App.css';
 import { RouterProvider } from 'react-router-dom';
-import { router } from './Routes/Routes';
+import { routes } from './Routes/Routes/Routes';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
-
-
     <div>
-      <RouterProvider router={router} >
- 
-      </RouterProvider>
+      <RouterProvider router={routes}></RouterProvider>
+      <Toaster></Toaster>
     </div>
   );
 }
--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,42 +1,28 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import { Outlet } from 'react-router-dom';
-import Header from './../Components/Shared/Header/Header';
-import RightSide from './../Components/Shared/RightSide/RightSide';
-import LeftSide from './../Components/Shared/LeftSide/LeftSide';
-import Footer from '../Components/Shared/Footer/Footer';
-
-
+import Footer from '../Pages/Shared/Footer/Footer';
+import Header from '../Pages/Shared/Header/Header';
+import LeftSideNav from '../Pages/Shared/LeftSideNav/LeftSideNav';
+import RightSideNav from '../Pages/Shared/RightSideNav/RightSideNav';
 
 const Main = () => {
     return (
         <div>
             <Header></Header>
-
             <Container>
-
-            <Row>
-                <Col lg='2'>
-                  
-                  <RightSide></RightSide>
-                      
-                </Col>
-
-                 <Col lg='7'>
-
-                 <Outlet></Outlet>
-                 </Col>
-
-
-                <Col lg='3'>
-
-                      <LeftSide></LeftSide>
-                </Col>
-            </Row>
-
-
+                <Row>
+                    <Col lg="2" className='d-none d-lg-block'>
+                        <LeftSideNav></LeftSideNav>
+                    </Col>
+                    <Col lg="7">
+                        <Outlet></Outlet>
+                    </Col>
+                    <Col lg="3">
+                        <RightSideNav></RightSideNav>
+                    </Col>
+                </Row>
             </Container>
-
             <Footer></Footer>
         </div>
     );
